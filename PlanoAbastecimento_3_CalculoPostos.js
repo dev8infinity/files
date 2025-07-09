2b364323f36ce21898b8a235804eaaeeb350e496
dev8infinity <dev8infinity@gmail.com>
2025-07-09 11:43:04 -0300

    teste - nao testado

--- PlanoAbastecimento_3_CalculoPostos.js
+++ PlanoAbastecimento_3_CalculoPostos.js
@@ -1,4 +1,6 @@
 function main() {
+  metadata.idCalculo = gerarId();
+
   const kmDeFuga = 300;
   const capacidadeTanque = Number(metadata.IcapacidadeTanque);
   const margemSeguranca = capacidadeTanque * 0.15;
@@ -13,27 +15,116 @@ function main() {
   }
 
   const capacidadeEfetivaTanque = capacidadeTanque - margemSeguranca;
-  const qtdNecessariaAbastecer = Math.ceil(distanciaViagem / consumoMedio - capacidadeEfetivaTanque);
-  if (qtdNecessariaAbastecer <= 0) {
+  const quantidadeNecessaria = Math.ceil(distanciaViagem / consumoMedio - capacidadeEfetivaTanque);
+  if (quantidadeNecessaria <= 0) {
+    metadata.erro = `Não houve necessidade de abastecimento na rota. `;
     return;
   }
+
   const listaPostoPorMenorPreco = postos.sort((a, b) => {
     if (a.precoLitro !== b.precoLitro) {
       return a.precoLitro - b.precoLitro;
     }
     return a.distanciaOrigem - b.distanciaOrigem;
   });
+  const abastecimentos = [];
+  let totalAbastecido = 0;
 
-  const {
-    totalAbastecido,
-    abastecimentos
-  } = calcularAbastecimento(qtdNecessariaAbastecer, listaPostoPorMenorPreco, 0, [], 0, 0);
+  const calcularAbastecimento = (qtdNecessariaAbastecer, indicePostoAtual = 0, minDistanciaOrigem = 0, maxDistanciaOrigem = Infinity) => {
+    if (qtdNecessariaAbastecer <= 0) {
+        return true; /** Já abastecemos o suficiente */
+    }
+    if (indicePostoAtual >= listaPostoPorMenorPreco.length) {
+        return false; /** Não há mais postos para abastecer */
+    }
+
+    /** posto mais barato */
+    const postoAtual = listaPostoPorMenorPreco[indicePostoAtual];
+
+    if (
+        postoAtual.distanciaOrigem <= minDistanciaOrigem ||
+        postoAtual.distanciaOrigem > maxDistanciaOrigem
+    ) {
+        /** ignora posto atrás do mínimo permitido ou além do máximo permitido */
+        return calcularAbastecimento(
+            qtdNecessariaAbastecer,
+            indicePostoAtual + 1,
+            minDistanciaOrigem,
+            maxDistanciaOrigem
+        );
+    }
+
+    /** quantidade de litros no tanque ao chegar no posto */
+    let litrosTanqueNoPosto =
+        capacidadeEfetivaTanque +
+        totalAbastecido -
+        postoAtual.distanciaOrigem / consumoMedio;
+
+    if (litrosTanqueNoPosto >= capacidadeEfetivaTanque) {
+        /** Já temos litros suficientes no tanque, não podemos abastecer neste posto, tentar o próximo mais barato */
+        return calcularAbastecimento(
+            qtdNecessariaAbastecer,
+            indicePostoAtual + 1,
+            minDistanciaOrigem,
+            maxDistanciaOrigem
+        );
+    }
+
+    if (litrosTanqueNoPosto < 0) {
+        /** abastecer em um posto com distancia menor que o atual para ter diesel para chegar nele */
+        const totalAbastecimentoAteAgora = totalAbastecido;
+        const abastecimentoAtePosto = calcularAbastecimento(
+            -litrosTanqueNoPosto,
+            indicePostoAtual + 1,
+            minDistanciaOrigem,
+            postoAtual.distanciaOrigem
+        );
+        if (abastecimentoAtePosto) {
+            /** Atualiza a quantidade necessária para abastecer */
+            qtdNecessariaAbastecer -= totalAbastecido - totalAbastecimentoAteAgora;
+            litrosTanqueNoPosto =
+                capacidadeEfetivaTanque +
+                totalAbastecido -
+                postoAtual.distanciaOrigem / consumoMedio;
+        } else {
+            return false; /** Não foi possível abastecer o suficiente para chegar ao próximo posto */
+        }
+    }
+
+    /** Calcula a quantidade de litros que podemos abastecer neste posto */
+    const litrosAbastecerNoPosto = Math.ceil(
+        Math.min(
+            qtdNecessariaAbastecer,
+            capacidadeEfetivaTanque - litrosTanqueNoPosto
+        )
+    );
+
+    /** Criar o abastecimento */
+    abastecimentos.push({
+        ...postoAtual,
+        km: postoAtual.distanciaOrigem,
+        litros: litrosAbastecerNoPosto,
+        preco: litrosAbastecerNoPosto * postoAtual.precoLitro,
+        completarTanque: (litrosTanqueNoPosto + litrosAbastecerNoPosto) >= capacidadeEfetivaTanque
+    });
+    qtdNecessariaAbastecer -= litrosAbastecerNoPosto;
+    totalAbastecido += litrosAbastecerNoPosto;
+
+    /** Chama recursivamente o próximo posto mais barato, filtrando os postos anteriores ao atual */
+    return calcularAbastecimento(
+        qtdNecessariaAbastecer,
+        indicePostoAtual + 1,
+        postoAtual.distanciaOrigem,
+        maxDistanciaOrigem
+    );
+  }
+
+  const abasteceuSuficiente = calcularAbastecimento(quantidadeNecessaria);
 
   metadata.Oerror = "";
   metadata.erro = metadata.erro || "";
-  metadata.idCalculo = gerarId();
 
-  checarErros(qtdNecessariaAbastecer, totalAbastecido, abastecimentos);
+  checarErros(abasteceuSuficiente, quantidadeNecessaria, totalAbastecido, abastecimentos);
   DataFile.Stage = 3;
 
   if (!metadata.erro) {
@@ -43,7 +134,7 @@ function main() {
     metadata.Oerror = JSON.stringify(
       {
         abastecimentos,
-        qtdNecessariaAbastecer,
+        quantidadeNecessaria,
         totalAbastecido,
       }
     );
@@ -51,121 +142,7 @@ function main() {
   }
 
   metadata.OpostosParaAbastecer = JSON.stringify(abastecimentos);
-  function calcularAbastecimento(
-    qtdNecessariaAbastecer,
-    listaPostoPorMenorPreco,
-    indicePostoAtual,
-    abastecimentos,
-    minDistanciaOrigem,
-    totalAbastecido,
-    maxDistanciaOrigem = Infinity
-  ) {
-    if (qtdNecessariaAbastecer <= 0) {
-      return {
-        totalAbastecido,
-        abastecimentos
-      }; /** Já abastecemos o suficiente */
-    }
-    if (indicePostoAtual >= listaPostoPorMenorPreco.length) {
-      return false; /** Não há mais postos para abastecer */
-    }
 
-    /** posto mais barato */
-    const postoAtual = listaPostoPorMenorPreco[indicePostoAtual];
-
-    if (
-      postoAtual.distanciaOrigem <= minDistanciaOrigem ||
-      postoAtual.distanciaOrigem > maxDistanciaOrigem
-    ) {
-      /** ignora posto atrás do mínimo permitido ou além do máximo permitido */
-      return calcularAbastecimento(
-        qtdNecessariaAbastecer,
-        listaPostoPorMenorPreco,
-        indicePostoAtual + 1,
-        abastecimentos,
-        minDistanciaOrigem,
-        totalAbastecido,
-        maxDistanciaOrigem
-      );
-    }
-
-    /** quantidade de litros no tanque ao chegar no posto */
-    let litrosTanqueNoPosto =
-      capacidadeEfetivaTanque +
-      totalAbastecido -
-      postoAtual.distanciaOrigem / consumoMedio;
-
-    if (litrosTanqueNoPosto >= capacidadeEfetivaTanque) {
-      /** Já temos litros suficientes no tanque, não podemos abastecer neste posto, tentar o próximo mais barato */
-      return calcularAbastecimento(
-        qtdNecessariaAbastecer,
-        listaPostoPorMenorPreco,
-        indicePostoAtual + 1,
-        abastecimentos,
-        minDistanciaOrigem,
-        totalAbastecido,
-        maxDistanciaOrigem
-      );
-    }
-
-    if (litrosTanqueNoPosto < 0) {
-      /** abastecer em um posto com distancia menor que o atual para ter diesel para chegar nele */
-      const abastecimentoAtePosto = calcularAbastecimento(
-        -litrosTanqueNoPosto,
-        listaPostoPorMenorPreco,
-        indicePostoAtual + 1,
-        abastecimentos,
-        minDistanciaOrigem,
-        totalAbastecido,
-        postoAtual.distanciaOrigem
-      );
-      if (abastecimentoAtePosto) {
-        /** Atualiza a quantidade necessária para abastecer */
-        qtdNecessariaAbastecer -=
-          abastecimentoAtePosto.totalAbastecido - totalAbastecido;
-        /** Atualiza o total abastecido */
-        totalAbastecido = abastecimentoAtePosto.totalAbastecido;
-        litrosTanqueNoPosto =
-          capacidadeEfetivaTanque +
-          totalAbastecido -
-          postoAtual.distanciaOrigem / consumoMedio;
-        /** Atualiza os abastecimentos */
-        abastecimentos = abastecimentoAtePosto.abastecimentos;
-      } else {
-        return false; /** Não foi possível abastecer o suficiente para chegar ao próximo posto */
-      }
-    }
-
-    /** Calcula a quantidade de litros que podemos abastecer neste posto */
-    const litrosAbastecerNoPosto = Math.ceil(
-      Math.min(
-        qtdNecessariaAbastecer,
-        capacidadeEfetivaTanque - litrosTanqueNoPosto
-      )
-    );
-
-    /** Criar o abastecimento */
-    abastecimentos.push({
-      ...postoAtual,
-      km: postoAtual.distanciaOrigem,
-      litros: litrosAbastecerNoPosto,
-      preco: litrosAbastecerNoPosto * postoAtual.precoLitro,
-      completarTanque: (litrosTanqueNoPosto + litrosAbastecerNoPosto) >= capacidadeEfetivaTanque
-    });
-    qtdNecessariaAbastecer -= litrosAbastecerNoPosto;
-    totalAbastecido += litrosAbastecerNoPosto;
-
-    /** Chama recursivamente o próximo posto mais barato, filtrando os postos anteriores ao atual */
-    return calcularAbastecimento(
-      qtdNecessariaAbastecer,
-      listaPostoPorMenorPreco,
-      indicePostoAtual + 1,
-      abastecimentos,
-      postoAtual.distanciaOrigem,
-      totalAbastecido,
-      maxDistanciaOrigem
-    );
-  }
 }
 function importar(abastecimentos) {
   if (abastecimentos.length == 0) {
@@ -198,8 +175,8 @@ function importar(abastecimentos) {
   );
 
 }
-function checarErros(qtdNecessariaAbastecer, totalAbastecido, abastecimentos) {
-  if (totalAbastecido < qtdNecessariaAbastecer) {
+function checarErros(abasteceuSuficiente, qtdNecessariaAbastecer, totalAbastecido, abastecimentos) {
+  if (!abasteceuSuficiente || totalAbastecido < qtdNecessariaAbastecer) {
     metadata.erro = metadata.erro + `Não há postos suficientes para chegar no destino/kmDeFuga. `;
   }
 
